aac8c33a02b208464129d2dcccd437948f9ef9a8
noah kim <heanokim@gmail.com>
2022-11-16 13:20:18 +0700

    Add files and changes required for deployment

--- app.js
+++ app.js
@@ -15,8 +15,9 @@ var app = express();
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
-const mongoDB =
+const dev_db_url =
   "mongodb+srv://hkburner:hkburner123!@cluster0.4pmdlyq.mongodb.net/local_library?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
